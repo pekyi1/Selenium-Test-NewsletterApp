4e5cdc3b9a767d8f9566b8b88c9ca0642e3ddf06
Fred Pekyi <fred.pekyi@amalitech.com>
2026-02-14 12:15:38 +0000

    added more test cased for better coverage

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -7,7 +7,10 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
+import java.time.Duration;
+
 public abstract class BaseTest {
+
     protected WebDriver driver;
 
     @BeforeEach
@@ -17,16 +20,25 @@ public abstract class BaseTest {
         boolean headless = Boolean.parseBoolean(System.getProperty("headless", "false"));
 
         ChromeOptions options = new ChromeOptions();
-        if (headless) options.addArguments("--headless=new");
+
+        if (headless) {
+            options.addArguments("--headless=new");
+        }
+
         options.addArguments("--window-size=1920,1080");
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
 
         driver = new ChromeDriver(options);
+
+        // optional: implicit wait for minor sync issues
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
     }
 
-//    @AfterEach
-//    void tearDown() {
-//        if (driver != null) driver.quit();
-//    }
+    @AfterEach
+    void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
 }
