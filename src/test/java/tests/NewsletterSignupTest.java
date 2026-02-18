7c3096a18ba30ba191be326ffd9d17600b9aaf5f
Fred Pekyi <fred.pekyi@amalitech.com>
2026-02-18 11:02:38 +0000

    check for error message for empty email

--- src/test/java/tests/NewsletterSignupTest.java
+++ src/test/java/tests/NewsletterSignupTest.java
@@ -38,7 +38,7 @@ public class NewsletterSignupTest extends BaseTest {
     void emptyEmail_showsError_andEmailInputGetsErrorClass() {
         NewsletterPage page = new NewsletterPage(driver)
                 .open(url)
-                .enterEmail("")
+                .enterEmail("r")
                 .clickSubscribeButton();
 
         Assertions.assertTrue(page.isEmailErrorDisplayed(), "Error message should display for empty email.");
